03560cc30300c96914bddbce7b974aee4815ae55
Mergal-Manuel <sup96445@gmail.com>
2025-03-12 01:33:53 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+<script>
+function Fare() {
+            const baseFare = 13;
+            var origin = document.getElementById('leaving').value;
+            var destination = document.getElementById('going').value;
+
+            var fare = baseFare + (destination - origin);
+            document.getElementById('ticketPrice').textContent = fare.toFixed(2);
+        }
+
+ function Discount() {
+            const baseFare = 13;
+            var origin = document.getElementById('leaving').value;
+            var destination = document.getElementById('going').value;
+
+            var regularFare = baseFare + (destination - origin);
+            var discount = regularFare * 0.8;
+            document.getElementById('ticketPrice').textContent = discount.toFixed(2);
+        }
+    </script>
